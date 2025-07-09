fdad326332464c1652d7ba8c5686785abe64cdfe
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-07-09 13:32:52 -0300

    chore: adicionando mudancas no package.json

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-const fs = require('fs');
-const trataErros = require('./erros/funcoesErros');
-
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2]; 
-
-fs.readFile(link, 'utf-8', (erro, texto) => {
-  try {
-    if (erro) throw erro
-    contaPalavras(texto); 
-    //todo o codigo que a gente quer que de certo, mas se der erro, queremos monitorar o códiog que está dentro desse bloco 
-  } catch (erro) {
-    console.log(trataErros(erro)); 
-
-  }
-})
 
 //ponto de entrada da aplicação, ponto inicial 
 function contaPalavras (texto) {
